7ec8c723846173c2fc07465371732c187c8018ce
jeffery <jeffery9@outlook.com>
2024-04-15 13:10:03 +0800

    keep working dirctory

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ branch=$(cat $cwd/ver)
 
 for f in $(cat $cwd/repos) 
 do  
+cd $cwd
 echo git subtree add --squash --prefix=$f  https://github.com/oca/$f $branch
 git subtree add --squash --prefix=$f  https://github.com/oca/$f $branch
 
