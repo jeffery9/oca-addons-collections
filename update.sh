d582e70c03ba709fd91ce1b54ccd5dfcd3edf1bf
jeffery <jeffery9@outlook.com>
2024-04-15 11:05:38 +0800

    init

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/sh 
+cwd=$(pwd)
+branch=$(cat $cwd/ver)
+
+for f in $(cat $cwd/repos) 
+do  
+echo git subtree pull --squash --prefix=$f  https://github.com/oca/$f $branch
+
+
+done 
+# git subtree 
