16ed0f79409d8dab62a3031ee4a3277748b44fe6
jeffery <jeffery9@outlook.com>
2024-04-15 13:17:07 +0800

    Squashed 'stock-logistics-request/' content from commit 59271c302e
    git-subtree-dir: stock-logistics-request
    git-subtree-split: 59271c302e3545b359f1ae3662fe85b99a75cd7e

---
+++ setup/_metapackage/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-stock-logistics-request",
+    description="Meta package for oca-stock-logistics-request Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-stock_request>=16.0dev,<16.1dev',
+        'odoo-addon-stock_request_mrp>=16.0dev,<16.1dev',
+        'odoo-addon-stock_request_purchase>=16.0dev,<16.1dev',
+        'odoo-addon-stock_request_submit>=16.0dev,<16.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 16.0',
+    ]
+)
