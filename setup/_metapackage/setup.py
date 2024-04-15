14a478d8d50eaaac12519f80db7f95533cdfc586
jeffery <jeffery9@outlook.com>
2024-04-15 13:11:37 +0800

    Squashed 'commission/' content from commit 0d1e564e0
    git-subtree-dir: commission
    git-subtree-split: 0d1e564e026b19be8bfe4bb09f1c8d8622420b3d

---
+++ setup/_metapackage/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-commission",
+    description="Meta package for oca-commission Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-account_commission>=16.0dev,<16.1dev',
+        'odoo-addon-commission>=16.0dev,<16.1dev',
+        'odoo-addon-commission_formula>=16.0dev,<16.1dev',
+        'odoo-addon-hr_commission>=16.0dev,<16.1dev',
+        'odoo-addon-sale_commission>=16.0dev,<16.1dev',
+        'odoo-addon-sale_commission_product_criteria>=16.0dev,<16.1dev',
+        'odoo-addon-sale_commission_salesman>=16.0dev,<16.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 16.0',
+    ]
+)
