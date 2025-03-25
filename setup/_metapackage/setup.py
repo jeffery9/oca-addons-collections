822a87f34c2b4ee000654059a7e050da8b40346a
jeffery <jeffery9@gmail.com>
2025-03-25 21:19:15 +0800

    Squashed 'fleet/' content from commit 9f2188396e
    git-subtree-dir: fleet
    git-subtree-split: 9f2188396ec889206a5af3075e8d6d4cd1f56111

---
+++ setup/_metapackage/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-fleet",
+    description="Meta package for oca-fleet Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-fleet_vehicle_calendar_year>=16.0dev,<16.1dev',
+        'odoo-addon-fleet_vehicle_category>=16.0dev,<16.1dev',
+        'odoo-addon-fleet_vehicle_configuration>=16.0dev,<16.1dev',
+        'odoo-addon-fleet_vehicle_fuel_capacity>=16.0dev,<16.1dev',
+        'odoo-addon-fleet_vehicle_fuel_type_ethanol>=16.0dev,<16.1dev',
+        'odoo-addon-fleet_vehicle_history_date_end>=16.0dev,<16.1dev',
+        'odoo-addon-fleet_vehicle_inspection>=16.0dev,<16.1dev',
+        'odoo-addon-fleet_vehicle_inspection_template>=16.0dev,<16.1dev',
+        'odoo-addon-fleet_vehicle_log_fuel>=16.0dev,<16.1dev',
+        'odoo-addon-fleet_vehicle_service_activity>=16.0dev,<16.1dev',
+        'odoo-addon-fleet_vehicle_service_calendar>=16.0dev,<16.1dev',
+        'odoo-addon-fleet_vehicle_service_kanban>=16.0dev,<16.1dev',
+        'odoo-addon-fleet_vehicle_service_services>=16.0dev,<16.1dev',
+        'odoo-addon-fleet_vehicle_stock>=16.0dev,<16.1dev',
+        'odoo-addon-fleet_vehicle_usage>=16.0dev,<16.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 16.0',
+    ]
+)
